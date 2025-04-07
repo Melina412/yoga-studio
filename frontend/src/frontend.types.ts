fc868ee4167a42087cc57a9fabfd0eaa185779a8
Melina412 <melina.schild@icloud.com>
2025-04-07 16:48:50 +0200

    zustand setup & login state handling

--- frontend/src/frontend.types.ts
+++ frontend/src/frontend.types.ts
@@ -1,9 +1,3 @@
-import { Dispatch, SetStateAction } from 'react';
-
-export interface LoginProps {
-  setLogin: Dispatch<SetStateAction<boolean>>;
-}
-
 export type ResponseType = { success: boolean; message: string; data?: any } | null;
 
 // Zustand
