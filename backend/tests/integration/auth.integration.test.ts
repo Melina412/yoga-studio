d8bc88b6fa74a69516a9e53d1ce457cfe570c114
Melina412 <melina.schild@icloud.com>
2025-06-03 10:40:42 +0200

    BE login route integration test

--- backend/tests/integration/auth.integration.test.ts
+++ backend/tests/integration/auth.integration.test.ts
@@ -24,3 +24,60 @@ describe('POST /api/auth/register', () => {
     expect(user?.email).toBe('newuser@example.com');
   });
 });
+
+//# POST /api/auth/login ------------------------------------------------------
+describe('POST /api/auth/login', () => {
+  //$ 1. user not found
+  it('should return 401 if email is not found', async () => {
+    const res = await request(app)
+      .post('/api/auth/login')
+      .send({ email: 'nonexistent@example.com', password: 'testpassword' });
+
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty('success', false);
+    expect(res.body).toHaveProperty('message', 'login failed: wrong login data');
+  });
+
+  //$ 2. incorrect password
+  it('should return 401 if password is incorrect', async () => {
+    const res = await request(app)
+      .post('/api/auth/login')
+      .send({ email: 'testuser@example.com', password: 'wrongpassword' });
+
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty('success', false);
+    expect(res.body).toHaveProperty('message', 'login failed: wrong login data');
+  });
+
+  //$ 3. successful login
+  it('should log in a user and set cookies', async () => {
+    const res = await request(app)
+      .post('/api/auth/login')
+      .send({ email: 'testuser@example.com', password: 'testpassword' });
+    // console.log(res.body);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('success', true);
+    expect(res.body).toHaveProperty('message', 'login successful');
+
+    let cookies = res.headers['set-cookie'] as unknown; // ts error bei string[]
+
+    // unknown wieder zu string[] konvertieren
+    if (typeof cookies === 'string') {
+      cookies = [cookies];
+    }
+
+    if (!Array.isArray(cookies)) {
+      throw new Error('no cookies set or wrong cookie format');
+    }
+    // console.log('cookies:', cookies);
+
+    expect(cookies).toBeDefined();
+    expect(cookies.length).toBeGreaterThanOrEqual(2);
+
+    const accessCookie = cookies.find((cookie: string) => cookie.startsWith('accessCookie='));
+    expect(accessCookie).toBeDefined();
+
+    const refreshCookie = cookies.find((cookie: string) => cookie.startsWith('refreshCookie='));
+    expect(refreshCookie).toBeDefined();
+  });
+});
